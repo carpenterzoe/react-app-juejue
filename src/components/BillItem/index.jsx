1a01d063161dfd0967245134f76a7074974f2078
carpenterzoe <carpenterzoe@outlook.com>
2023-12-10 15:11:53 +0800

    feat: 每日账单list的item

--- src/components/BillItem/index.jsx
+++ src/components/BillItem/index.jsx
@@ -16,23 +16,18 @@ const BillItem = ({ bill }) => {
   const [expense, setExpense] = useState(0); // 支出
   const navigateTo = useNavigate(); // 路由实例
 
-  // 当添加账单是，bill.bills 长度变化，触发当日收支总和计算。
+  // 当添加账单时，bill.bills 长度变化，触发当日收支总和计算。
   useEffect(() => {
-    // 初始化将传入的 bill 内的 bills 数组内数据项，过滤出支出和收入。
-    // pay_type：1 为支出；2 为收入
-    // 通过 reduce 累加
     const _income = bill.bills.filter(i => i.pay_type == 2).reduce((curr, item) => {
       curr += Number(item.amount);
       return curr;
     }, 0);
     setIncome(_income);
-    console.log('_income: ', _income);
     const _expense = bill.bills.filter(i => i.pay_type == 1).reduce((curr, item) => {
       curr += Number(item.amount);
       return curr;
     }, 0);
     setExpense(_expense);
-    console.log('_expense: ', _expense);
   }, [bill.bills]);
 
   // 前往账单详情
@@ -70,10 +65,10 @@ const BillItem = ({ bill }) => {
               <span>{item.type_name}</span>
             </>
           }
-          description={
+          suffix={
             <span style={{ color: item.pay_type == 2 ? 'red' : '#39be77' }}>{`${item.pay_type == 1 ? '-' : '+'}${item.amount}`}</span>
           }
-          help={<div>{dayjs(Number(item.date)).format('HH:mm')} {item.remark ? `| ${item.remark}` : ''}</div>}
+          description={<div>{dayjs(Number(item.date)).format('HH:mm')} {item.remark ? `| ${item.remark}` : ''}</div>}
         >
         </List.Item>)
       }
